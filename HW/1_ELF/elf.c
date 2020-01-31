ab30f6e11eca4f95feba3ec8554416219c0d7b2e
Justin <jw6858@cs.ship.edu>
2020-01-31 17:39:22 -0500

    can detect both PE and ELF files

--- HW/1_ELF/elf.c
+++ HW/1_ELF/elf.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define NUM_MAGIC_BYTES 1
+#define NUM_MAGIC_BYTES 4
 
 int isELF (char *bytes)
 {
+    if (strstr(bytes, "ELF") != NULL) {
+        return 1;
+    }
     return 0;
 }
 
 int isPE (char *bytes)
 {
+    if (strstr(bytes, "MZ") != NULL) {
+        return 1;
+    }
     return 0;
 }
 
 int main (int argc, char **argv)
 {
     char bytes[NUM_MAGIC_BYTES];
+
     if (argc != 2)
     {
         printf("Error - must give me a file name\n");
@@ -24,20 +32,21 @@ int main (int argc, char **argv)
 
     printf("Checking magic on file named %s\n", argv[1]);
 
-    //fopen(file on argv[1])
+    FILE *fp = fopen(argv[1], "r");
 
-    //fread(NUM_MAGIC_BYTES)
+    fread(bytes, NUM_MAGIC_BYTES, 1, fp);
 
-    if (isELF(bytes))
-    {
+    if (isELF(bytes)) {
         printf("File is ELF\n");
     }
-    else if (isPE(bytes))
-    {
+    else if (isPE(bytes)) {
         printf("File is PE\n");
     }
+    else {
+        printf("File is neither ELF nor PE\n");
+    }
 
-    //fclose(file)
+    fclose(fp);
 
     return 0;
 }
