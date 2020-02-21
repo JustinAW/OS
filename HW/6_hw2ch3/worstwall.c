20e05cf116085c7470c03b415afb764ee4a97406
Justin Weigle <jw6858@cs.ship.edu>
2020-02-21 01:46:41 -0500

    done with number 2 i suppose

--- HW/6_hw2ch3/worstwall.c
+++ HW/6_hw2ch3/worstwall.c
@@ -1,6 +1,6 @@
 /************************************************
  *               worstwall.c                    *
- * Uses shared memory to replicate the ... 2nd  *
+ * Uses shared memory to replicate the 2nd      *
  * worst facebook wall ever                     *
  ************************************************
  * Author: Justin Weigle                        *
@@ -13,32 +13,37 @@
 #include <sys/mman.h>
 #include <sys/wait.h>
 
+#define PAGESIZE 4096
+
 int main(int argc, char **argv)
 {
     char choice;
-    char messg[128];
-    int status;
+    char userin[4096];
 
-    int leng = 128;
-    int protec = PROT_READ | PROT_WRITE;
-    int flgs = MAP_SHARED | MAP_ANONYMOUS;
-    void *shrmem = mmap(NULL, leng, protec, flgs, -1, 0);
+    /***** allocate the shared memory *****/
+    char *shrmem = mmap(NULL, PAGESIZE, PROT_READ | PROT_WRITE,
+            MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 
     while (1) {
-        printf("Would you like to set your status? y/n?\n");
+        printf("---------------\nCurrent status:\n---------------\n");
+        /***** print the contents of the shared memory *****/
+        printf("%s", shrmem);
+        printf("---------------\n");
+        printf("Would you like to change your status? y/n?\n");
         choice = getchar();
-        fflush(stdin);
+        getchar(); // eat newline, nom nom
 
         if (choice == 'y') {
             printf("You chose yes\nNew status?:\n");
-            scanf("%s", &messg);
-            fflush(stdin);
-            memcpy(shrmem, messg, sizeof(messg));
-            messg[0] = '\0';
-            choice = '\0';
-        } else {
+            fgets(userin, sizeof(userin), stdin);
+            /***** store the users input in the shared memory *****/
+            memcpy(shrmem, userin, sizeof(userin));
+        } else if (choice == 'n') {
             printf("You chose no\n");
             exit(0);
+        } else {
+            printf("Not a valid choice, press Enter to continue\n");
+            while (getchar() != '\n') {}
         }
     }
 
